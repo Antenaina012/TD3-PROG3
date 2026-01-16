fa128f38fd19d806af8a57606f17a0d906527d75
Antenaina012 <hei.antenaina.7@gmail.com>
2026-01-16 16:55:53 +0300

    feat: added Dish.java

---
+++ src/main/java/org/example/Dish.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.List;
+
+public class Dish {
+    private int id;
+    private String name;
+    private String dishType;
+    private List<Ingredient> ingredients; // Liste des ingrédients associés
+
+    public Dish(int id, String name, String dishType) {
+        this.id = id;
+        this.name = name;
+        this.dishType = dishType;
+    }
+}
