106da651dd97e8a0c70f511f271085afc27a0710
Antenaina012 <hei.antenaina.7@gmail.com>
2026-01-16 16:53:48 +0300

    Correction de la structure et ajout des fichiers de l'exercice

--- src/main/java/org/example/Ingredient.java
+++ src/main/java/org/example/Ingredient.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Ingredient {
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+
+    // Constructeurs, Getters et Setters
+    public Ingredient(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 }
